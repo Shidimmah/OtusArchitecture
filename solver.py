efa3c051bf4f8057765f82ae612e46825f0a9c8f
Shidimmah <Shibboleth=2>
2024-10-13 18:06:26 +0300

    Финальная версия ДЗ

--- solver.py
+++ solver.py
@@ -1,5 +1,18 @@
 import math
 
 def solve(a, b, c):
-    return []  # возвращаем пустой список
+    if any(math.isnan(x) or math.isinf(x) for x in [a, b, c]):
+        raise ValueError("коэффициенты должны быть конечными.")
 
+    if abs(a) < 1e-9:
+        raise ValueError("коэффициент а не может быть нулём (0)")
+
+    D = b ** 2 - 4 * a * c
+    if D > 0:
+        x1 = (-b + math.sqrt(D)) / (2 * a)
+        x2 = (-b - math.sqrt(D)) / (2 * a)
+        return [x1, x2]
+    elif D == 0:
+        x = -b / (2 * a)
+        return [x, x]
+    return []
--- unit tests/test_solver.py
+++ unit tests/test_solver.py
@@ -1,4 +1,10 @@
 from solver import solve
 
 def test_no_roots():
-    assert solve(1, 0, 1) == []  #тест отсутствия корней
+    assert solve(1, 0, 1) == []
+
+def test_two_roots():
+    assert solve(1, 0, -1) == [1, -1]
+
+def test_one_root():
+    assert solve(1, 2, 1) == [-1, -1]
